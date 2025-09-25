b6a5e8cc287775da98e61247d7d784b243125ec5
ignacioLopezIDLI <ignaciolopez2000Q@gmail.com>
2025-09-25 18:06:38 +0200

    Integración de Kinde con autenticación, navbar dinámica y lógica de usuario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
-import { Check, Phone, Star } from "lucide-react";
+import { Check, Phone as PhoneIcon, Star } from "lucide-react";
+import Phone from "./components/Phone";
 
 export default function Home() {
   return (
@@ -87,34 +88,54 @@ export default function Home() {
                 </div>
 
                 <div className="flex flex-col justify-between items-center sm:items-start">
-                  <div className=" flex gap-0.5"> 
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600
-                    fill-green-600" />
+                  <div className=" flex gap-0.5">
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
+                    <Star
+                      className="h-4 w-4 text-green-600
+                    fill-green-600"
+                    />
                   </div>
-                  <p> <span className=" font-semibold">1.250</span> Clientes 
-                  felices</p>
+                  <p>
+                    {" "}
+                    <span className=" font-semibold">1.250</span> Clientes
+                    felices
+                  </p>
                 </div>
-
               </div>
             </div>
           </div>
 
-          <div className="col-span-full lg:col-span-1 w-full flex justify-center
-          px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+          <div
+            className="col-span-full lg:col-span-1 w-full flex justify-center
+          px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit"
+          >
             <div className="relative md:max-w-xl">
-              <img src="/your-image.png" className="absolute w-40 lg:w-52
-              left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"/>
-              <img src="/line.png" className=" absolute w-20 -left-6 -bottom-6
-              select-none"/>
-              <Phone/>
+              <img
+                src="/your-image.png"
+                className="absolute w-40 lg:w-52
+              left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+              />
+              <img
+                src="/line.png"
+                className=" absolute w-20 -left-6 -bottom-6
+              select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
