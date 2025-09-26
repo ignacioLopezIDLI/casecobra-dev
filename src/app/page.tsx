43d7ebca359ee01c38a1ab849362bab421716d63
ignacioLopezIDLI <ignaciolopez2000Q@gmail.com>
2025-09-26 13:29:44 +0200

    Implementación de sistema de testimonios con imágenes y cuadrícula responsiva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
 import { Check, Phone as PhoneIcon, Star } from "lucide-react";
 import Phone from "./components/Phone";
+import { Icons } from "./components/icons";
+import { Reviews } from "./components/Reviews";
 
 export default function Home() {
   return (
@@ -139,6 +141,95 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+
+      {/* Propuesta de valor */}
+      <section className=" bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance 
+            !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              Que dicen nuestros{" "}
+              <span className="relative px-2">
+                Clientes<Icons.Underline className="hidden sm:block
+                pointer-events-none absolute inset-x-0 -bottom-6 text-green-500"
+                />
+              </span>{" "}
+
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2"></img>
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1  px-4 lg:mx-0
+            lg:max-w-none lg:grid-cols-2 gap-y-16">
+              <div className=" flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                <div className="flex gap-0.5 mb-2">
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                </div>
+                <div className="text-lg leading-8">
+                  <p>
+                    "CaseCobra transformó mi teléfono en una obra de arte
+                    personalizada.<span className="p-0.4 bg-slate-800 text-white">La calidad es excepcional</span> y el proceso fue
+                    sencillo. ¡Recomiendo encarecidamente sus servicios!"
+                  </p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <img className="rounded-full h-12 w-12 object-cover"
+                  src="/users/user-1.png"
+                  alt="user"> 
+                  </img>
+                  <div className="flex flex-col">
+                    <p className="font-semibold">Roberto</p>
+                    <div className="flex gap-1.5 items-center text-zinc-600">
+                      <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                      <p className="text-sm">Compra Verificada</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* Second testimonial */}
+              
+              <div className=" flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                <div className="flex gap-0.5 mb-2">
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                  <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                </div>
+                <div className="text-lg leading-8">
+                  <p>
+                    "Estoy encantado con mi carcasa personalizada de CaseCobra.
+                    <span className="p-0.5 bg-slate-800 text-white">El diseño es único</span> y la protección que ofrece es excelente.
+                    ¡Mi teléfono nunca se vio tan bien!"
+                  </p>
+                </div>
+                <div className="flex gap-4 mt-2">
+                  <img className="rounded-full h-12 w-12  object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"> 
+                  </img>
+                  <div className="flex flex-col">
+                    <p className="font-semibold">Joaquin</p>
+                    <div className="flex gap-1.5 items-center text-zinc-600">
+                      <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                      <p className="text-sm">Compra Verificada</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </MaxWidthWrapper>
+
+
+        <div className="pt-16">
+          <Reviews/>
+        </div>
       </section>
     </div>
   );
