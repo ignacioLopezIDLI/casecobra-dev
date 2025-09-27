5c7db3339cec018b589b92821905ab66167dcfbd
ignacioLopezIDLI <ignaciolopez2000Q@gmail.com>
2025-09-27 21:06:35 +0200

    Componentes Reviews y Footer listos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
-import { Check, Phone as PhoneIcon, Star } from "lucide-react";
+import { ArrowRight, Check, Phone as PhoneIcon, Star } from "lucide-react";
+import Link from 'next/link'
 import Phone from "./components/Phone";
 import { Icons } from "./components/icons";
 import { Reviews } from "./components/Reviews";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -129,8 +131,8 @@ export default function Home() {
             <div className="relative md:max-w-xl">
               <img
                 src="/your-image.png"
-                className="absolute w-40 lg:w-52
-              left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                className="absolute w-40 lg:w-30
+              left-45 -top-25 select-none hidden sm:block lg:hidden xl:block"
               />
               <img
                 src="/line.png"
@@ -231,6 +233,79 @@ export default function Home() {
           <Reviews/>
         </div>
       </section>
+
+      <section>
+        <MaxWidthWrapper>
+             <div className="mb-12 px-6 lg:px-8">
+         <div className="mx-auto max-w-2xl sm:text-center" >
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance 
+            !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              Sube tu foto y obtiene{" "}
+              <span className="relative px-2 bg-green-600 text-white">
+                Tu propia Carcasa 
+                </span>{" "}
+                   Ahora
+            </h2>
+          </div> 
+        </div>
+
+        <div className="mx-auto max-w-6xl px-6 lg:px-8" >
+          <div className=" relative flex flex-col items-center md:grid
+          grid-cols-2 gap-40">
+            <img src="arrow.png" className="absolute top-[25rem] md:top-1/2 
+            -translate-y-1-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+             />
+
+
+            <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset
+            ring-gray-900/10 lg:rounded-2xl">
+              <img src="/horse.jpg" className="rounded-md object-cover
+              bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"/>
+            </div>
+
+            <Phone className="w-60" imgSrc="/horse_phone.jpg"/>
+
+          </div>
+        </div>
+
+            <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+              <li className="w-fit">
+                  <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
+                  Sube tu foto favorita
+              </li>
+              <li className="w-fit">
+                  <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
+                  Resistente a golpes y arañazos
+              </li>
+              <li className="w-fit">
+                  <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
+                  Compatible con carga inalambrica
+              </li>
+              <li className="w-fit">
+                  <Check className="h-5 w-5 text-green-600 inline mr-1.5"/>
+                  5 años de garantia
+              </li>
+
+              
+                  <div className="flex justify-center"> 
+                    <Link className={buttonVariants({
+                      size: "lg",
+                      className: "mx-auto mt-8"
+                    })} href='/configure/upload'>
+                    Crea tu propia Carcasa <ArrowRight className="h-4 w-4 ml-1.5"
+                      /> 
+                      
+                      </Link>
+                  </div>
+              
+            
+
+            
+            </ul>
+        </MaxWidthWrapper>
+       
+      </section>
+
     </div>
   );
 }
